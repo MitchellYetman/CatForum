6676f330df97636b728eb0b2c44055eb395369f1
Mitchell Yetman <w0498497@nscc.ca>
2025-02-09 19:57:33 -0400

    home page task compelted

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,20 +1,38 @@
 using System.Diagnostics;
+using CatForum.Data;
 using CatForum.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CatForum.Controllers
 {
     public class HomeController : Controller
     {
-       
-        public HomeController()
+        private readonly CatForumContext _context;
+
+        public HomeController(CatForumContext context)
         {
-           
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var discussions = await _context.Discussion.ToListAsync();
+
+            //sort syntax from ChatGPT
+            discussions.Sort((x, y) => y.CreateDate.CompareTo(x.CreateDate));
+
+            //syntax for dictionary and fetch, as well as idea to use dictionary, from ChatGPT. my original thought was to pass an object that held DiscussionId's and comment counts
+            var discussionComments = new Dictionary<int, int>();
+            foreach(var d in discussions)
+            {
+                var commentCount = await _context.Comment.Where(c => c.DiscussionId == d.DiscussionId).CountAsync();
+                discussionComments.Add(d.DiscussionId, commentCount);
+            }
+
+            ViewBag.DiscussionComments = discussionComments;
+
+            return View(discussions);
         }
 
         public IActionResult Privacy()
@@ -22,6 +40,11 @@ namespace CatForum.Controllers
             return View();        
         }
 
-      
+        public IActionResult GetDiscussion()
+        {
+            return View();
+        }
+
+
     }
 }
